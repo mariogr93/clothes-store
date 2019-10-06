b76c9820579240089cd97333ac1bccedd9e2de7c
mariogr93 <mariogr93@gmail.com>
2019-10-05 22:00:30 -0400

    added SASS library to the project, started working with the homepage setup and views

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return (
-    <div>
-        hola
-    </div>
-  );
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
