8138008e3c9d639d76d8a6ea9f61e37c1d80dbdd
mariogr93 <mariogr93@gmail.com>
2019-10-07 11:12:12 -0400

    Sign In and Sign In form added with custom button component

---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./signin-signup.style.scss";
+
+import SignIn from "../../components/signin/signin.component";
+
+const SigninAndSignup = () => <div className='signin-and-signup'><SignIn/></div>;
+
+export default SigninAndSignup;
