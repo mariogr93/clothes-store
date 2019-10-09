65c5deb31a707919b6c355feda13afb2d574cb4d
mariogr93 <mariogr93@gmail.com>
2019-10-09 00:28:56 -0400

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
 	apiKey: "AIzaSyCoJAsZGzALHea91_5Pt1-E_420ruZbwic",
@@ -15,6 +15,31 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log("error creating user", error.message);
+		}
+	}
+
+	return userRef;
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
@@ -23,4 +48,3 @@ provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
-
--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import "./signin-signup.style.scss";
 
 import SignIn from "../../components/signin/signin.component";
+import SignUp from '../../components/signup/signup.component';
 
-const SigninAndSignup = () => <div className='signin-and-signup'><SignIn/></div>;
+const SigninAndSignup = () => <div className='signin-and-signup'><SignIn/><SignUp/></div>;
 
 export default SigninAndSignup;
